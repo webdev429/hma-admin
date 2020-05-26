f1c3b3a276c9474950be8048d8732eafb20a451e
zoran-web <zoran.cuckovic102@hotmail.com>
2020-05-27 07:49:07 +0800

    added loading spinner

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -585,6 +585,47 @@
   .collapse-body div.dropdown.bootstrap-select {
     width: 100% !important;
   }
+  .loader,
+  .loader:after {
+    border-radius: 50%;
+    width: 10em;
+    height: 10em;
+  }
+  .loader {
+    margin: 60px auto;
+    font-size: 10px;
+    position: relative;
+    text-indent: -9999em;
+    border-top: 1.1em solid rgba(255, 255, 255, 0.2);
+    border-right: 1.1em solid rgba(255, 255, 255, 0.2);
+    border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
+    border-left: 1.1em solid #333333;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation: load8 1.1s infinite linear;
+    animation: load8 1.1s infinite linear;
+  }
+  @-webkit-keyframes load8 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes load8 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
 </style>
 @endsection
 
@@ -986,6 +1027,10 @@
     });
 
     function sendAjaxRequest() {
+      $("#listArea").empty();
+      var loadingSpinner = "<div class='loader'>Loading...</div>";
+      $("#listArea").append(loadingSpinner);
+
       $.ajax({
         type: "POST",
         url: "ajax_get_deals_with_filter",
